65fc03173674d28a9c23091ce725dfb6b3a176fd
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-02-04 05:41:23 -0600

    initital commit - project creation

---
+++ randomEats/randomEats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  randomEats
+//
+//  Created by Nicholas Coggin on 2/4/19.
+//  Copyright © 2019 Nicholas Coggin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
