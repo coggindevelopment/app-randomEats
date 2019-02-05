79b80d78c4ca9d7c832daf0fe7fd17e93064559d
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-02-05 05:54:39 -0600

    randomEats02 commit - app finished

--- randomEats/randomEats/ViewController.swift
+++ randomEats/randomEats/ViewController.swift
@@ -24,9 +24,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        restaurantsStackView.isHidden = true
+        randomRestaurantStackView.isHidden = true
+    }
+    
+    @IBAction func whereToEatBtnWasPressed(_ sender: Any) {
+        randomRestaurantLabel.text = restaurants.randomElement()
+        youShouldEatAtLabal.isHidden = false
+        randomRestaurantLabel.isHidden = false
+    }
+    
+    func updateRestaurantsLabel() {
+        restaurantsLabel.text = restaurants.joined(separator: ", ")
     }
 
-
+    @IBAction func addRestaurantBtnWasPressed(_ sender: Any) {
+        guard let restaurantName = addRestaurantTextField.text else { return }
+        
+        if addRestaurantTextField.text == "" {
+            // Do nothing
+        } else {
+            restaurants.append(restaurantName)
+            updateRestaurantsLabel()
+            addRestaurantTextField.text = ""
+            restaurantsStackView.isHidden = false
+            
+            if restaurants.count > 1 {
+                randomRestaurantStackView.isHidden = false
+                youShouldEatAtLabal.isHidden = true
+                randomRestaurantLabel.isHidden = true
+            }
+        }
+    }
+    
 }
 
